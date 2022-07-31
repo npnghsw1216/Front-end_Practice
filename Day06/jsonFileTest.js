a1fe4d8a9ed02301deb362be4bcfadd41db30954
npnghsw1216 <npnghsw1216@naver.com>
2022-07-31 20:52:05 +0900

    Day06_jsonFileTest.js

--- Day06/jsonFileTest.js
+++ Day06/jsonFileTest.js
@@ -8,7 +8,7 @@ user.password = "1111";
 
 userJSON = JSON.stringify(user);
 
-/*
+
 // 파일입출력은 기본적으로 비동기방식
 // writeFile(경로, 작성할 내용, 인코딩방식, 콜백함수(에러))
 file.writeFile('user.json', userJSON, 'UTF-8', 
@@ -20,9 +20,10 @@ file.writeFile('user.json', userJSON, 'UTF-8',
             console.log("파일 write 성공");
         }
     });
-*/
+
 
 // readFile(경로, 인코딩, 콜백함수(에러, 내용))
+/*
 file.readFile('./user.json', 'UTF-8',
     function(e, data){
         let user = JSON.parse(data);
@@ -30,3 +31,4 @@ file.readFile('./user.json', 'UTF-8',
         console.log('아이디 : %s', user.id);
         console.log(user.pw);
     });
+*/
