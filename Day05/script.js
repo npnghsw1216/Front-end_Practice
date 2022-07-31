5cdf10d3dcc6ff11ae93d7e3f9be162abff8b7e4
npnghsw1216 <npnghsw1216@naver.com>
2022-07-31 20:25:29 +0900

    Day05_script.js

---
+++ Day05/script.js
@@ -0,0 +1,16 @@
+/*
+    Artist : sungwoo Heo
+    Content : Mouse Event
+    Date : 2022. 7. 30
+*/
+function over(arg){
+    arg.src = "images/color.png";
+}
+
+function out(arg){
+    arg.src = "images/grayscale.png";
+}
+
+function dbclick(arg){
+    arg.src = "images/ori.png";
+}
